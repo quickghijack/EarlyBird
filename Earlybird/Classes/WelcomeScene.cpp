30a3f1c1d6811a593c6837bc8b057695774b72d6
quickghijack <xiecong@xiecongdeMacBook-Pro.local>
2016-04-13 20:10:19 +0800

    2016.4.12
    4.12

---
+++ Earlybird/Classes/WelcomeScene.cpp
@@ -0,0 +1,9 @@
+//
+//  WelcomeScene.cpp
+//  Earlybird
+//
+//  Created by 谢从 on 16/4/13.
+//
+//
+
+#include "WelcomeScene.h"
---
+++ Earlybird/Classes/WelcomeScene.h
@@ -0,0 +1,15 @@
+//
+//  WelcomeScene.hpp
+//  Earlybird
+//
+//  Created by zhe on 16/4/13.
+//
+//
+
+#ifndef WelcomeScene_hpp
+#define WelcomeScene_hpp
+
+#include "cocos2d.h"
+
+
+#endif /* WelcomeScene_hpp */
